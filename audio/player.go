bef2212029fdd28fcb4696ec12f3c4e8f80a8e34
Alban Seurat <alban.seurat@me.com>
2021-07-23 14:39:19 +0200

    portaudio : use callback to have better timing precision

--- audio/player.go
+++ audio/player.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"github.com/gordonklaus/portaudio"
 	"goplay2/globals"
 	"goplay2/ptp"
 	"io"
@@ -15,7 +16,6 @@ type PlaybackStatus uint8
 
 const (
 	STOPPED PlaybackStatus = iota
-	STARTED
 	PLAYING
 )
 
@@ -23,10 +23,16 @@ var underflow = errors.New("audio underflow")
 
 type Stream interface {
 	io.Closer
-	Init() error
-	Write([]int16) error
+	Init(callBack func(out []int16, info portaudio.StreamCallbackTimeInfo)) error
 	Start() error
 	Stop() error
+	CurrentTime() time.Duration
+}
+
+type audioSyncInfo struct {
+	firstFrameTime      int64
+	firstFrameTimestamp int64
+	previousDrift       int64
 }
 
 type Player struct {
@@ -35,34 +41,44 @@ type Player struct {
 	Status         PlaybackStatus
 	stream         Stream
 	ringBuffer     *Ring
-	nextStart      int64
-	nextFrame      int64
+	audioSync      audioSyncInfo
 	syncLogger     *log.Logger
 }
 
 func NewPlayer(syncFile io.Writer, clock *ptp.VirtualClock, ring *Ring) *Player {
-
 	return &Player{
 		clock:          clock,
 		ControlChannel: make(chan globals.ControlMessage, 100),
 		stream:         NewStream(),
 		Status:         STOPPED,
 		ringBuffer:     ring,
-		nextStart:      0,
+		audioSync:      audioSyncInfo{},
 		syncLogger:     log.New(syncFile, "SYNC: ", log.Ldate|log.Ltime|log.Lshortfile),
 	}
 }
 
+func (p *Player) callBack(out []int16, info portaudio.StreamCallbackTimeInfo) {
+	drift := p.clock.Now().Add(-p.stream.CurrentTime()).UnixNano()
+	log.Printf("call back timing info : %v now : %v , clock :%v diff : %v\n",
+		info, p.stream.CurrentTime(), p.clock.Now().UnixNano(), p.audioSync.previousDrift-drift)
+	frame, err := p.ringBuffer.TryPop()
+	if err == ErrIsEmpty {
+		p.fillSilence(out)
+	} else {
+		err = binary.Read(bytes.NewReader(frame.(*PCMFrame).pcmData), binary.LittleEndian, out)
+		if err != nil {
+			globals.ErrLog.Printf("error reading data : %v\n", err)
+		}
+	}
+	p.audioSync.previousDrift = drift
+}
+
 func (p *Player) Run() {
 	var err error
-
-	syncA := syncAudio{}
-	if err := p.stream.Init(); err != nil {
+	if err := p.stream.Init(p.callBack); err != nil {
 		globals.ErrLog.Fatalln("Audio Stream init error:", err)
 	}
 	defer p.stream.Close()
-	out := make([]int16, 2048)
-
 	for {
 		select {
 		case msg := <-p.ControlChannel:
@@ -76,54 +92,24 @@ func (p *Player) Run() {
 				}
 				p.Status = STOPPED
 			case globals.START:
-				p.Status = STARTED
-				p.nextStart = msg.Param1
-				p.nextFrame = msg.Param2
-			case globals.SKIP:
-				p.skipUntil(msg.Param1, msg.Param2)
-			}
-		default:
-			if p.Status != STOPPED && p.clock.Now().UnixNano() >= p.nextStart {
-				if p.Status == STARTED {
-					p.Status = PLAYING
-					p.syncLogger.Printf("%v Starting streaming with anchor time %v at %v\n", time.Now(), p.nextStart, p.clock.Now().UnixNano())
+				if p.Status == STOPPED {
 					err = p.stream.Start()
 					if err != nil {
 						globals.ErrLog.Printf("error while starting flow : %v\n", err)
 						return
 					}
 				}
-				frame := p.ringBuffer.Pop().(*PCMFrame)
-				// https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.125.8673&rep=rep1&type=pdf
-				p.syncLogger.Printf("Frame timestamp : %v , now : %v - skew ratio: %v\n",
-					frame.Timestamp, p.clock.Now().UnixNano(), syncA.audioSkew(p.clock.Now().UnixNano(), int64(frame.Timestamp)))
-				err = binary.Read(bytes.NewReader(frame.pcmData), binary.LittleEndian, out)
-				if err != nil {
-					globals.ErrLog.Printf("error reading data : %v\n", err)
-					return
-				}
-				if err = p.stream.Write(out); err != nil {
-					globals.ErrLog.Printf("error writing audio :%v\n", err)
-					if err := p.stream.Stop(); err != nil {
-						globals.ErrLog.Printf("error stopping audio :%v\n", err)
-						return
-					}
-					p.Status = STARTED
-				}
-			} else {
-				// yield while there is no playing
-				time.Sleep(50 * time.Millisecond)
+				p.Status = PLAYING
+				p.audioSync.firstFrameTime = msg.Param1
+				p.audioSync.firstFrameTimestamp = msg.Param2
+			case globals.SKIP:
+				p.skipUntil(msg.Param1, msg.Param2)
 			}
 		}
 	}
 }
 
 func (p *Player) skipUntil(fromSeq int64, UntilSeq int64) {
-	if p.Status == PLAYING {
-		p.syncLogger.Printf("Stopping streaming for skipping to sequence %v\n", UntilSeq)
-		p.stream.Stop()
-		p.Status = STOPPED
-	}
 	p.ringBuffer.Flush(func(value interface{}) bool {
 		frame := value.(*PCMFrame)
 		return frame.SequenceNumber < uint32(fromSeq) || frame.SequenceNumber > uint32(UntilSeq)
@@ -136,4 +122,10 @@ func (p *Player) Push(frame interface{}) {
 
 func (p *Player) Reset() {
 	p.ringBuffer.Reset()
+}
+
+func (p *Player) fillSilence(out []int16) {
+	for i := range out {
+		out[i] = 0
+	}
 }
